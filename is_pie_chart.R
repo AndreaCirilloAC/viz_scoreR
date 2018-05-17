6e496cc729ca976133851ea5295ff8c26b608a4b
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-05-17 18:36:21 +0200

    added is_pie_chart function

--- is_pie_chart.R
+++ is_pie_chart.R
@@ -1,8 +1,12 @@
 is_pie_chart <- function(plot_object, n_of_layers){
 
+  coordinates_class <-   plot_object$coordinates %>% class() %>% as.vector()  
+  
   # if we do not found any geom_bar we can be sure it is not a pie chart
   
-!is.na(match("GeomBar",geoms_lister(plot_object,n_of_layers)))
+!is.na(match("GeomBar",geoms_lister(plot_object,n_of_layers))) & match("CoordPolar",coordinates_class)>0
+
+
 
   }
 
