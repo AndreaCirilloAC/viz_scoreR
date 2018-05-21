b839ef812167cbedb2ee749f745d4b203152f7f4
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-05-21 21:35:47 +0200

    working version of histogram bins tester function

--- histogram_bins_tester.R
+++ histogram_bins_tester.R
@@ -1,16 +1,25 @@
-histogram_bins_tester <- function(plot_objects){
+histogram_bins_tester <- function(plot_objects, n_of_layers){
 # first of all I look for a GeomBar layer and a binwidth parameter. the occurence of 
   # both means we are "looking" at an histogram ( there is no separate geom)
   bar_index <- match("GeomBar", geoms_lister(plot_object,n_of_layers))
-  bar_stat_params <- if(bar_index > 0){plot_object$layers[[1]]$stat_params %>% names()}
+  bar_stat_params <- if(bar_index > 0){plot_object$layers[[bar_index]]$stat_params %>% names()}
   test_histogram <- bar_index > 0 & match("binwidth",bar_stat_params)
   
-  # if(test_histogram){
-  #   p_build$data[[bar_index]]
-  #   theoretical_binwidth <-     
-  #}
+  if(test_histogram){
+aes_db     <- mappings_lister(plot_object , n_of_layers )
+variable_name   <- aes_db %>% filter(aes == "x") %>% select("variable") %>% pull()
+variable_vector <- plot_object$data[,variable_name]
+optimal_bw       <-  2 * (IQR(variable_vector) / length(variable_vector)^(1/3)) 
+actual_bw_index  <- match("binwidth",bar_stat_params)
+actual_bw          <- plot_object$layers[[bar_index]]$stat_params[[actual_bw_index]] 
+if (is.null(actual_bw)){
+  actual_bw <- diff(range(variable_vector))/default_n_of_bins
+  } # we handle here the common case of the user not changing the default setting for bins size
 
+distance_from_optimum <- optimal_bw - actual_bw
+optimization_data <- list("optimal_bw" = optimal_bw, "distance_from_optimum" = distance_from_optimum)
+return(optimization_data)  
+  }else{FALSE}
   
-    
-#  bw <- diff(range(x)) / (2 * IQR(x) / length(x)^(1/3))  
+
 }
