1737799aab84e9b73330510338d416468e4ebfff
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-05-24 17:20:28 +0200

    fixed logical error on too_much_dim...

--- R/too_much_dimensions.R
+++ R/too_much_dimensions.R
@@ -2,7 +2,7 @@ too_much_dimensions <- function(plot_object,n_of_layers){
 # we check here if there is a combination of geom_point/ geom_line and mapping = size
 
 point_or_line <- match(c("GeomLine", "GeomPoint"),geoms_lister(plot_object,n_of_layers)) %>% 
-  sum(na.rm = TRUE)>0
+  sum(na.rm = TRUE) %>% is.na()
 mapped_on_size <- match("size",
                         mappings_lister(plot_object,n_of_layers) )>0
 return(point_or_line & mapped_on_size)
