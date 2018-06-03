0bc8c1c4b39656b1ca6d5115f594362ed8166d5e
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-06-03 21:55:52 +0200

    fixed issue with aes puller, column selection

--- R/aes_puller.R
+++ R/aes_puller.R
@@ -2,7 +2,7 @@ aes_puller <- function(plot_object,n_of_layers, required_aes){
   
   aes_db     <- mappings_lister(plot_object , n_of_layers )
   variable_name   <- aes_db %>% filter(aes == required_aes) %>% select("variable") %>% pull()
-  col_index <- match(variable_name,aes_db$variable)
+  col_index <- match(variable_name,colnames(plot_object$data))
   if(length(variable_name)== 0 ){return(NA)}else{
   variable_vector <- plot_object$data[,col_index] 
 
