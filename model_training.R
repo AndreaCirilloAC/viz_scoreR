c67a3881edb617cd4c9be4bbd5d78be5a9c1c8e1
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-07-13 22:07:12 +0200

    added tune grid

--- model_training.R
+++ model_training.R
@@ -1,31 +1,47 @@
 library(caret)
 library(skimr)
+library(plyr)
+estimation_db$good_plot <- revalue(estimation_db$good_plot , c("0"="bad", "1"="good"))
+
+estimation_db  %>%
+          select(-plot_name)-> final_db
   
+
+
+
 createDataPartition(
-  y = estimation_db$good_plot,
+  y = final_db$good_plot,
   ## the outcome data are needed
-  p = .80,
+  p = .95,
   ## The percentage of data in the
   ## training set
   list = FALSE) -> train_subset
 
-training <- estimation_db[ train_subset,-1] #remove the plot name column
-testing  <- estimation_db[-train_subset,-1]
+
+training <- final_db[ train_subset,-1] #remove the plot name column
+testing  <- final_db[-train_subset,-1]
 
 nrow(training)
 
 nrow(testing)
 
-control <-  trainControl(interaction.depth = 9)
+control <-  trainControl( classProbs = TRUE)
+
+gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9), 
+                        n.trees = (1:30)*50, 
+                        shrinkage = 0.1,
+                        n.minobsinnode = 20)
 
 gbm_fit <- train(  good_plot~.,
   data = training,
   method = "gbm",
-#  trControl = control,
-  verbose = FALSE
-)
+trControl = control,
+  verbose = FALSE,
+tuneGrid = gbmGrid)
 
 plot(gbm_fit)
 
-#http://topepo.github.io/caret/model-training-and-tuning.html#fitting-models-without-parameter-tuning
-
+#aggiungi questi:
+#  http://www.cookbook-r.com/Graphs/Plotting_distributions_(ggplot2)/ ca 16
+#http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/ 13
+#http://www.cookbook-r.com/Graphs/Scatterplots_(ggplot2)/ 11
