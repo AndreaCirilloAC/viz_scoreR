2880e01a0e8e1b287b2d7e03cae2600f82bc29a8
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-11-01 22:13:23 +0100

    working report and correct handling of external data

--- vizscrorer/R/scorer_bot.R
+++ vizscrorer/R/scorer_bot.R
@@ -3,69 +3,69 @@ scorer_bot <- function(plot_object = NULL){
  plot_metadata <- data.frame(area_label = unlist(plot_metadata_raw$area_label),
                              topic_label = unlist(plot_metadata_raw$topic_label),
                              test        = unlist(plot_metadata_raw$test),
-                             ispositive = unlist(plot_metadata_raw$ispositive)) 
+                             ispositive = unlist(plot_metadata_raw$ispositive))
 
   score <- scorer(plot_metadata)
- 
+
   score_map <- data.frame(lower = c(0,0.11,0.41,0.71),
                           upper = c(0.10,0.40,0.70,1),
                           judgment = c("poorly", "improvable","good!", "great!"))
 
   score_map$score_match <- mapply(between,
-                                  x = rep(score,nrow(score_map)), 
+                                  x = rep(score,nrow(score_map)),
                                   left = score_map$lower,
                                   right = score_map$upper,
                                   SIMPLIFY = TRUE)
-  score_map %>% 
-    filter(score_match == TRUE) %>% 
-    select(judgment) %>% pull() %>% 
+  score_map %>%
+    filter(score_match == TRUE) %>%
+    select(judgment) %>% pull() %>%
     as.character() -> judgment
 
-
-
-  # from metadata, leveraging area_label and ispositive attribute, we highligh which area of dataviz
+  # from metadata, leveraging area_label and ispositive attribute, we highlight which area of dataviz
   # shows problem for the plot
   # we will highlight those area to our user
+  # brief test for histograms to filter out test note relevant for non-histogram plots
   is_histogram <- test_for_histogram(plot_object,plot_object$layers %>% length())
 
-  plot_metadata %>% 
-  {if (is_histogram == FALSE) filter(., topic_label != "number_of_bins" & topic_label != "flipped_barplot" ) else filter(.,topic_label !="stop")}  %>% 
-    select(area_label,ispositive) %>% 
-    group_by(area_label,ispositive) %>% 
-    count() %>% 
-    spread(key = ispositive, value = n) %>% 
+  plot_metadata %>%
+  {if (is_histogram == FALSE) filter(., topic_label != "number_of_bins" & topic_label != "flipped_barplot" ) else filter(.,topic_label !="stop")}  %>%
+    select(area_label,ispositive) %>%
+    group_by(area_label,ispositive) %>%
+    count() %>%
+    spread(key = ispositive, value = n) %>%
     mutate(positive_ratio = `TRUE`/sum(`TRUE`,`FALSE`, na.rm = TRUE)) %>%
-    mutate(area_ratio     =round(positive_ratio,2)) %>% 
-    rename(passed = `TRUE`,failed = `FALSE`) %>% 
+    mutate(area_ratio     =round(positive_ratio,2)) %>%
+    rename(passed = `TRUE`,failed = `FALSE`) %>%
     mutate(total = sum(passed , failed,na.rm = TRUE)) -> positive_ratio_db
-  
-  positive_ratio_db %>% 
+
+  positive_ratio_db %>%
     select(area_label,failed,passed,area_ratio) -> positive_ratio_show
-  
-  positive_ratio_show[is.na(positive_ratio_show)] <- 0 
-  
-  positive_ratio_db %>% 
-    filter(positive_ratio == min(.$positive_ratio)) %>% 
-    select(area_label) %>% 
-    pull() %>% 
+
+  positive_ratio_show[is.na(positive_ratio_show)] <- 0
+
+  positive_ratio_db %>%
+    filter(positive_ratio == min(.$positive_ratio)) %>%
+    select(area_label) %>%
+    pull() %>%
     as.character() -> worst_area # the worst area was the one in which the plot obtained ther worst positive rate,
   #i.e. number of positive results in test given the overall number of test for that area.
-  
+
   # we then select each specific test showing problems, so to give specific advice for each of this elements.
-  plot_metadata %>% 
-  {if (is_histogram == FALSE) filter(., topic_label != "number_of_bins" & topic_label != "flipped_barplot" ) else filter(.,topic_label !="stop")}  %>% 
-    select(topic_label,ispositive) %>% 
+  plot_metadata %>%
+  {if (is_histogram == FALSE) filter(., topic_label != "number_of_bins" & topic_label != "flipped_barplot" ) else filter(.,topic_label !="stop")}  %>%
+    select(topic_label,ispositive) %>%
     filter(ispositive == FALSE) -> errors_db
-  
+
   # merge errors_db with a db storing a suggestion for each possible error
-  advices_db <- read.csv("plot_advices.csv", sep = ";", stringsAsFactors = FALSE)
-  
-  errors_db %>% 
+  advices_path <- system.file("extdata", "plot_advices.csv", package = "vizscrorer")
+  advices_db <- read.csv(advices_path, sep = ";", stringsAsFactors = FALSE)
+
+  errors_db %>%
     left_join(.,advices_db,by = "topic_label") -> teaching_db
 
 n_of_errors <- nrow(errors_db)
-
-file.copy("report.Rmd", to = "plot_report.Rmd",overwrite = TRUE)
+report_path <- system.file("extdata", "report.Rmd", package = "vizscrorer")
+file.copy(report_path, to = "plot_report.Rmd",overwrite = TRUE)
 write(" ", file = "plot_report.Rmd", append = TRUE)
 for (i in 1:n_of_errors){
 
@@ -86,8 +86,8 @@ for (i in 1:n_of_errors){
  write("  " , file = "plot_report.Rmd", append = TRUE)
  write("_your dataviz bot advisor_", file = "plot_report.Rmd", append = TRUE)
  write("  " , file = "plot_report.Rmd", append = TRUE)
- 
-  rmarkdown::render("plot_report.Rmd")
+
+  rmarkdown::render("plot_report.Rmd",  revealjs_presentation())
   system("open plot_report.html")
 }
 
